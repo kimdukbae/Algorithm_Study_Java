1697b5787fad0f14f4a70a7f279f3513d29c62e5
kimdukbae <han885404@naver.com>
2021-02-25 15:21:49 +0900

    2021

---
+++ src/com/company/프로그래머스/그리디/구명보트.java
@@ -0,0 +1,4 @@
+package com.company.프로그래머스.그리디;
+
+public class 구명보트 {
+}
--- src/com/company/프로그래머스/그리디/큰수만들기.java
+++ src/com/company/프로그래머스/그리디/큰수만들기.java
@@ -2,13 +2,37 @@ package com.company.프로그래머스.그리디;
 
 public class 큰수만들기 {
 
-    static String number = "1924";
-    static int k = 2;
-    static String answer = "";
-
     public static void main(String[] args) {
 
-        
+        System.out.println(solution("1924", 2));
+        System.out.println(solution("1231234", 3));
+        System.out.println(solution("4177252841", 4));
 
+    }
+
+    public static String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        int answer_Length = number.length() - k;
+
+        int start = 0;
+        int end = number.length() - answer_Length;
+        int idx = 0;
+
+        while (answer_Length > 0) {
+            int max = -1;
+            for (int i = start; i <= end; i++) {
+                int num = number.charAt(i) - '0';
+                if (num > max) {
+                    max = num;
+                    idx = i;
+                }
+            }
+            start = idx + 1;
+            answer_Length--;
+            end = number.length() - answer_Length;
+            sb.append(max);
+        }
+
+        return sb.toString();
     }
 }
