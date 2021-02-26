5bdfbd8bbe41be13f6d452cb5610d4411b0fc1e6
kimdukbae <han885404@naver.com>
2021-02-26 12:35:10 +0900

    2021

---
+++ src/com/company/프로그래머스/그리디/섬연결하기.java
@@ -0,0 +1,17 @@
+package com.company.프로그래머스.그리디;
+
+public class 섬연결하기 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int n, int[][] costs) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+}
